8bf5ff3fad76e30b5114df12d406f07b9fa0da67
Richart_Griffin <richard.griffin.artist@gmail.com>
2023-04-18 20:31:57 -0400

    operacion eliminar valor final

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ bl_info = {
     "description": "Descripcion del add-on",
 }
 number_list = [""]
-numer_active = 0
+number_active = 0
 last_result = 0
 last_operation = ""
 
@@ -22,9 +22,9 @@ class WriteOneOperator(bpy.types.Operator):
     string_property: StringProperty(name="Input String",description="Enter a string",default="",)
     def execute(self, context):
         global number_list
-        global numer_active
+        global number_active
         global last_result
-        number_list[numer_active] = number_list[numer_active] + self.string_property
+        number_list[number_active] = number_list[number_active] + self.string_property
         last_result = number_list[0]
         print("press to " + self.string_property)
         return {'FINISHED'}
@@ -37,12 +37,12 @@ class NewOperator(bpy.types.Operator):
     string_property: StringProperty(name="Operator String",description="Enter a string",default="",)
     def execute(self, context):
         global number_list
-        global numer_active
+        global number_active
         global last_operation
         if number_list[-1] == "":
             number_list[-2] = str(self.string_property)
         else:
-            numer_active = numer_active + 2
+            number_active = number_active + 2
             number_list.append(self.string_property)
             number_list.append("")
         return {'FINISHED'}
@@ -55,49 +55,85 @@ class EqualOperator(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
     def execute(self, context):
         global number_list
-        global numer_active
+        global number_active
         global last_result
-        numer_active = numer_active
-        ref_indice = 0      
-        if numer_active == 0:
+        number_active = number_active
+        ref_indice = 0
+        if number_active == 0:
             print("igual al mismo numero")
+            return {'FINISHED'}
+        if number_active == 2 and number_list[-1] == "":
+            number_list = number_list[:-2]
+            return {'FINISHED'}
         else:
-            if number_list[-2] == "+" or number_list[-2] == "-" or number_list[-2] == "*" or number_list[-2] == "/" and number_list[-1] == "" :
-                number_list = number_list[:-2]
+            continuar = True
             for indice in number_list:
                 ref_indice = ref_indice + 1
                 print(str(indice))
-                if indice == "+" :
+                #if number_list[-2] == "+" or number_list[-2] == "-" or number_list[-2] == "*" or number_list[-2] == "/" and number_list[-1] == "" :
+                #    number_list = number_list[:-2]
+                #    return {'FINISHED'}
+                if indice == "+" and continuar:
                     last_result = int(last_result) + int(number_list[ref_indice])
                     print("suma :" + str(last_result))
                         
-                elif indice == "-":
+                elif indice == "-"and continuar:
                     last_result = int(last_result) - int(number_list[int(ref_indice)])
                     print("Resta :" + str(last_result))
                     
-                elif indice == "*":
+                elif indice == "*"and continuar:
                     last_result = int(last_result) * int(number_list[int(ref_indice)])
                     print("multiplicacion")
                     
-                elif indice == "/":
+                elif indice == "/"and continuar:
                     last_result = int(last_result) / int(number_list[int(ref_indice)])
                     print("division")
                     
             print("numero activo :" + str(last_result))
         number_list.clear()
         number_list.append(str(last_result))
-        numer_active = 0
+        number_active = 0
         return {'FINISHED'}
-
 # Clear Operations
 class ClearOperator(bpy.types.Operator):
     bl_idname = "object.clear"
     bl_label = "Botón Clear"
     bl_options = {'REGISTER', 'UNDO'}
     def execute(self, context):
+        global number_active
         number_list.clear()
         number_list.append("")
-        numer_active = 0
+        number_active = 0
+        return {'FINISHED'}
+
+# Clear last number Operations
+class ClearLasNumberOperator(bpy.types.Operator):
+    bl_idname = "object.clear_last_number"
+    bl_label = "Botón Clear Last Number"
+    bl_options = {'REGISTER', 'UNDO'}
+    def execute(self, context):
+        global number_list
+        global number_active
+        if number_active == 0 and number_list[0] == "":
+            print("igual al mismo numero")
+            return {'FINISHED'}
+        elif number_active == 0 and number_list[0] != "":
+            ref_i = str(number_list[-1])
+            if len(ref_i) > 0:
+                ref_i = ref_i[:-1]
+                number_list[-1] = ref_i
+        elif number_list[-2] == "+" or number_list[-2] == "-" or number_list[-2] == "*" or number_list[-2] == "/" and number_list[-1] == "" :
+            number_list = number_list[:-2]
+            ref_longitud = len(number_list) - 1
+            number_active = ref_longitud
+            return {'FINISHED'}
+        else:
+            ref_i = str(number_list[-1])
+            if len(ref_i) > 0:
+                ref_i = ref_i[:-1]
+                number_list[-1] = ref_i
+            ref_longitud = len(number_list) - 1
+            number_active = ref_longitud
         return {'FINISHED'}
 
 # interfaces
@@ -110,38 +146,40 @@ class CalculatorPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        
+
         box = layout.box()
         row = box.row()
-        split = row.split(factor=1/5)
-        split.operator( "object.equal_operator", text="=")
+        split = row.split(factor=5/6)
         split.label(text = "".join(number_list) )
+        split.operator( "object.clear_last_number", text="", icon='TRIA_LEFT')
 
         row = layout.row(align=True)
-        row.operator("object.new_operator", text="+").string_property = "+"
-        row.operator("object.new_operator", text="-").string_property = "-"
-        row.operator("object.new_operator", text="*").string_property = "*"
-        row.operator("object.new_operator", text="/").string_property = "/"
+        split = row.split(factor=1/4)
+        split.operator("object.write_number", text="7").string_property = "7"
+        split.operator("object.write_number", text="8").string_property = "8"
+        split.operator("object.write_number", text="9").string_property = "9"
+        split.operator("object.new_operator", text="+").string_property = "+"
         
         row = layout.row(align=True)
-        row.operator("object.write_number", text="7").string_property = "7"
-        row.operator("object.write_number", text="8").string_property = "8"
-        row.operator("object.write_number", text="9").string_property = "9"
+        split = row.split(factor=1/4)
+        split.operator("object.write_number", text="4").string_property = "4"
+        split.operator("object.write_number", text="5").string_property = "5"
+        split.operator("object.write_number", text="6").string_property = "6"
+        split.operator("object.new_operator", text="-").string_property = "-"
         
         row = layout.row(align=True)
-        row.operator("object.write_number", text="4").string_property = "4"
-        row.operator("object.write_number", text="5").string_property = "5"
-        row.operator("object.write_number", text="6").string_property = "6"
-        
-        row = layout.row(align=True)
-        row.operator("object.write_number", text="1").string_property = "1"
-        row.operator("object.write_number", text="2").string_property = "2"
-        row.operator("object.write_number", text="3").string_property = "3"
+        split = row.split(factor=1/4)
+        split.operator("object.write_number", text="1").string_property = "1"
+        split.operator("object.write_number", text="2").string_property = "2"
+        split.operator("object.write_number", text="3").string_property = "3"
+        split.operator("object.new_operator", text="*").string_property = "*"
 
         row = layout.row(align=True)
-        split = row.split(factor=2/3)
-        split.operator( "object.write_number", text="0").string_property = "0"
+        split = row.split(factor=1/4)
         split.operator( "object.clear", text="", icon='TRASH')
+        split.operator( "object.write_number", text="0").string_property = "0"
+        split.operator( "object.equal_operator", text="=")
+        split.operator("object.new_operator", text="/").string_property = "/"
         
 
 
@@ -151,6 +189,7 @@ def register():
     bpy.utils.register_class(NewOperator)
     bpy.utils.register_class(EqualOperator)
     bpy.utils.register_class(ClearOperator)
+    bpy.utils.register_class(ClearLasNumberOperator)
 
 def unregister():
     bpy.utils.unregister_class(CalculatorPanel)
@@ -158,6 +197,7 @@ def unregister():
     bpy.utils.unregister_class(NewOperator)
     bpy.utils.unregister_class(EqualOperator)
     bpy.utils.unregister_class(ClearOperator)
+    bpy.utils.unregister_class(ClearLasNumberOperator)
 
 if __name__ == "__main__":
     register()
